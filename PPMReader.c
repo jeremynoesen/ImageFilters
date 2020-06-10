a630d6092ac26fb5ac2920aa72a0f16a1a3ca70d
Jeremy Noesen <jeremynoesen@gmail.com>
2020-06-10 00:26:13 -0500

    Tested PPM reader

--- PPMReader.c
+++ PPMReader.c
@@ -2,38 +2,45 @@
 #include <string.h>
 #include "PPMReader.h"
 
-/* Color struct to manage pixel colors */
+/* Pixel struct to manage pixel colors */
 typedef struct {
-    int limit, r, g, b;
-} Color;
+    unsigned int limit, r, g, b;
+} Pixel;
 
-/* Read a PPM image to a Color array */
-struct Color *readPPMImage(const char *fileName) {
+/* Read a PPM image to a Pixel array */
+struct Pixel *readPPMImage(const char *fileName) {
     FILE *file = fopen(fileName, "r");
-    char *fileType = "";
-    fscanf(file, "%s", fileType);
+    char *fileType;
+    fscanf(file, " %s", fileType);
 
     if (strcmp(fileType, "P6") != 0) return 0;
 
     int width, height, limit;
     fscanf(file, " %d %d %d", &width, &height, &limit);
 
-    Color imgArray[width][height];
+    Pixel imgArray[width][height];
 
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            int r, g, b;
+            if(feof(file)) break;
+            unsigned int r, g, b;
             fscanf(file, " %d %d %d", &r, &g, &b);
-            Color color;
-            color.r = r;
-            color.g = g;
-            color.b = b;
-            color.limit = limit;
-            imgArray[x][y] = color;
+            Pixel pixel;
+            pixel.limit = limit;
+            pixel.r = r > limit ? limit : r;
+            pixel.g = g > limit ? limit : g;
+            pixel.b = b > limit ? limit : b;
+            imgArray[x][y] = pixel;
         }
     }
 
-    fclose(fileType);
+    fclose(file);
 
     return imgArray;
+}
+
+/* Test file loading */
+int main(void) {
+    printf("test");
+    readPPMImage("icon.ppm");
 }
