2d222a1d77cb0bd73224727311559e0a4d360f3e
Jeremy Noesen <jeremynoesen@gmail.com>
2020-06-09 22:10:35 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/GOLFilter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GOLFilter.dir/main.c.o"
+  "GOLFilter"
+  "GOLFilter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GOLFilter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
